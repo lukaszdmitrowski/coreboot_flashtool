506d1f41ff76925b8a18e736dccaaf719ae26e23
Lukasz <lukasz.dmitrowski@gmail.com>
2015-06-10 01:24:15 +0200

    [ADD] Added use of fl_supported_flash_chips

--- supported.cpp
+++ supported.cpp
@@ -13,7 +13,7 @@ Supported::Supported(QWidget *parent) :
     ui(new Ui::Supported)
 {
     ui->setupUi(this);
-    QStandardItemModel *model = new QStandardItemModel(2,3,this); //2 Rows and 3 Columns
+    /*QStandardItemModel *model = new QStandardItemModel(2,3,this); //2 Rows and 3 Columns
 
     model->setHorizontalHeaderItem(0, new QStandardItem(QString("Column1 Header")));
     model->setHorizontalHeaderItem(1, new QStandardItem(QString("Column2 Header")));
@@ -22,11 +22,15 @@ Supported::Supported(QWidget *parent) :
     ui->tableView->setModel(model);
 
     QStandardItem *firstRow = new QStandardItem(QString("ColumnValue"));
-    model->setItem(0,0,firstRow);
+    model->setItem(0,0,firstRow);*/
 
-    fl_flashchip_t const **myflashchip;
-    fl_supported_flash_chips(myflashchip);
-    qDebug() << (*myflashchip)->vendor;
+    unsigned int flashchip_info_size = fl_supported_flash_chips_size();
+    fl_flashchip_info_t *flashchip_info;
+    flashchip_info = new fl_flashchip_info_t[flashchip_info_size];
+    flashchip_info[0].name = "test";
+    fl_supported_flash_chips(&flashchip_info);
+
+    //delete [] flashchip_info;
 }
 
 Supported::~Supported()
