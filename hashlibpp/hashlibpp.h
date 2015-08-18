7cd54d04fd0c5c98d4cadf20593867d79a504cb2
Lukasz Dmitrowski <lukasz.dmitrowski@gmail.com>
2015-08-18 17:02:53 +0200

    [DEL] Removed not needed files

--- hashlibpp/hashlibpp.h
+++ hashlibpp/hashlibpp.h
@@ -47,12 +47,7 @@
 #include "hl_exception.h"
 #include "hl_wrapperfactory.h"
 #include "hl_hashwrapper.h"
-#include "hl_md5wrapper.h"
-#include "hl_sha1wrapper.h"
 #include "hl_sha256wrapper.h"
-#include "hl_sha384wrapper.h"
-#include "hl_sha512wrapper.h"
-
 
 //----------------------------------------------------------------------
 #endif
